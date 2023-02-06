372ef2cbc0414f96536acfd9ece836c2ab7e83bf
YashasBoddu <124089868+YashasBoddu@users.noreply.github.com>
2023-02-06 09:35:39 -0600

    Create  StatefulWidget.dart

---
+++ StatefulWidget.dart
@@ -0,0 +1,14 @@
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+ 
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+ 
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
